0c43a90b51f0c23634b45f659dcd853702af3a90
DecBassel <bassel444555@gmail.com>
2023-06-18 16:49:28 +0300

    fix to deploy

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "pro") {
   app.use(express.static(path.join(__dirname, "../client", "build")));
 
   app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
